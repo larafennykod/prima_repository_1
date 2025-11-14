71aaf180fc261a898b3242c3be0ef3874fc513ef
larafennykod <lara.filippone@kodland.team>
2025-11-14 18:16:20 +0100

    Create curiosita.py
    ho creto uno script di curiosità

---
+++ curiosita.py
@@ -0,0 +1,11 @@
+import random
+
+curiosita = [
+    "Ho tre cani!",
+    "Adoro la pizza",
+    "Guardo un sacco di serie TV"
+]
+
+curiosita_random = random.choice(curiosita)
+
+print(curiosita_random)
